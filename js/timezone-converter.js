5463b09d8e8a3c199ab9269b503975c8a2296237
Iain MacDonald <IJMacD@gmail.com>
2014-08-12 19:04:33 +0800

    show correct timezones

--- js/timezone-converter.js
+++ js/timezone-converter.js
@@ -7,13 +7,27 @@ $(function(){
 		dateTextNow = $('#date-text-now'),
 
 		currentDate,
-		currentDateClone;
-	
+		currentDateLocal,
+		currentDateUTC,
+
+		zoneRegex = /[+-]\d\d:?\d\d/;
+
 	dateInput.on("keyup change", function(){
-		currentDate = moment(dateInput.val());
-		updateDateText();
+		var val = dateInput.val(),
+			zone = findZone(val);
+		if(val.length){
+			currentDate = moment(new Date(val));
+			currentDateLocal = currentDate.clone();
+			currentDateUTC = currentDate.clone().zone(0);
+			if(zone){
+				currentDate.zone(zone[0]);
+			}
+			updateDateText();
+		}
 	});
-		
+
+	dateTextNow.attr("data-label", moment().format("z"));
+
 	setInterval(updateNowText, 500);
 
 	function updateNowText(){
@@ -24,14 +38,33 @@ $(function(){
 		if(!currentDate){
 			return;
 		}
-		
-		currentDateClone = currentDate.clone();
-		
-		dateTextLocal.text(currentDate.format());
-		
-		currentDateClone.tz("UTC");
-		dateTextUTC.text(currentDateClone.format());
-		
+
+		dateTextOriginal.text(currentDate.format());
+		dateTextOriginal.attr("data-label", formatOffset(currentDate.zone()));
+
+		dateTextLocal.text(currentDateLocal.format());
+		dateTextLocal.attr("data-label", currentDateLocal.format("Z"));
+
+		dateTextUTC.text(currentDateUTC.format());
+		dateTextUTC.attr("data-label", currentDateUTC.format("Z"));
+
 		dateTextRelative.text(currentDate.fromNow());
+	}
+
+	function findZone(str){
+		var match = str.match(zoneRegex);
+		return match;
+	}
+
+	function formatOffset(minutes){
+		var neg = -minutes < 0,	// offset is other way round
+			abs = Math.abs(minutes),
+			hours = Math.floor(abs / 60),
+			mins = abs - (hours * 60);
+		return (neg?"-":"+") + pad(hours) + ":" + pad(mins);
+	}
+
+	function pad(n){
+		return (n<10)?"0"+n:n;
 	}
 });
